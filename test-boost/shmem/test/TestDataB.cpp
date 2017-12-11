a9a5f230d1c01fdb6dd1003a9e32cf7f60680845
Netz <Netz@home>
2017-12-11 01:14:46 +0100

    random generator

--- test-boost/shmem/test/TestDataB.cpp
+++ test-boost/shmem/test/TestDataB.cpp
@@ -1,5 +1,8 @@
 #include <string>
 
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include "boost/date_time/gregorian/gregorian_types.hpp"
+
 #include "TestDataB.h"
 #include "rashm/rashm_traits.h"
 #include "shmem/shmem_traits.h"
@@ -18,11 +21,14 @@ bool TestDataB::operator==( TestDataB const & d ) const {
 
 template<>
 TestGenerator<TestDataB>::TestGenerator() : current { 789, 22, 5792 } {
-
+    e.seed((boost::posix_time::microsec_clock::universal_time()-boost::posix_time::ptime(boost::gregorian::date(1970,1,1))).total_nanoseconds());
+    current.l2 = randlong();
+    current.l3 = randlong();
 }
 
 template<>
 TestDataB TestGenerator<TestDataB>::next() {
+    current.l1 = randlong();
     current.l2++;
     current.l3--;
     return current;
--- test-boost/shmem/test/TestGenerator.h
+++ test-boost/shmem/test/TestGenerator.h
@@ -8,11 +8,20 @@
 #ifndef SHMEM_TEST_TESTGENERATOR_H_
 #define SHMEM_TEST_TESTGENERATOR_H_
 
+#include <random>
+
 template<typename DATA>
 struct TestGenerator {
     TestGenerator();
     DATA next();
     DATA current;
+
+    long randlong() {
+        std::uniform_int_distribution<long> d{100,1000};
+        return d(e);
+    }
+
+    std::default_random_engine e;
 };
 
 
