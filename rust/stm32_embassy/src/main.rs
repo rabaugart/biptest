ec62934eeeeaf48911b2f6a64d052ee842c798f2
Netz <netz@Rabookmi>
2025-08-31 14:15:07 +0200

    Steuere Intervall mit Button

--- rust/stm32_embassy/src/main.rs
+++ rust/stm32_embassy/src/main.rs
@@ -2,17 +2,18 @@
 #![no_main]
 
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::gpio::{Level, Input, Output, Pull, Speed};
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _}; // global logger
 
 #[embassy_executor::task]
-async fn blinker(mut led: Output<'static>, interval: Duration) {
+async fn blinker(mut led: Output<'static>, button: Input<'static>, interval: Duration) {
     loop {
+        let ivb = if button.is_high() { interval } else { interval*2 };
         led.set_high();
-        Timer::after(interval).await;
+        Timer::after(ivb).await;
         led.set_low();
-        Timer::after(interval).await;
+        Timer::after(ivb).await;
     }
 }
 
@@ -21,5 +22,6 @@ async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
     let led = Output::new(p.PB3, Level::Low, Speed::Low);
-    let _ = spawner.spawn(blinker(led, Duration::from_millis(1_000)));
+    let button = Input::new(p.PB4,Pull::Down);
+    let _ = spawner.spawn(blinker(led, button, Duration::from_millis(1_000)));
 }
