bf0ffd147e89e896670bd0247f05f24124f5a96a
Netz <netz@Rabookmi>
2025-08-31 10:12:17 +0200

    Probiere Rust auf stm32 mit embassys. Funktioniert.

---
+++ rust/stm32_embassy/readme.md
@@ -0,0 +1,7 @@
+# Rust auf stm32 mit embassy
+
+31.8.2025
+
+Flashen mit `probe-rs` über
+
+    cargo run --release
---
+++ rust/stm32_embassy/src/main.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::{Duration, Timer};
+use {defmt_rtt as _, panic_probe as _}; // global logger
+
+#[embassy_executor::task]
+async fn blinker(mut led: Output<'static>, interval: Duration) {
+    loop {
+        led.set_high();
+        Timer::after(interval).await;
+        led.set_low();
+        Timer::after(interval).await;
+    }
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+
+    let led = Output::new(p.PB3, Level::Low, Speed::Low);
+    spawner.spawn(blinker(led, Duration::from_millis(1_000)));
+}
