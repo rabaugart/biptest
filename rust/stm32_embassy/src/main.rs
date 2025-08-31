94261a9c15ba0a179750e053755248ef4c99e1f6
Netz <netz@Rabookmi>
2025-08-31 14:26:50 +0200

    Gebe Meldung beim Start aus

--- rust/stm32_embassy/src/main.rs
+++ rust/stm32_embassy/src/main.rs
@@ -1,15 +1,20 @@
 #![no_std]
 #![no_main]
 
+use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Input, Output, Pull, Speed};
+use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _}; // global logger
 
 #[embassy_executor::task]
 async fn blinker(mut led: Output<'static>, button: Input<'static>, interval: Duration) {
     loop {
-        let ivb = if button.is_high() { interval } else { interval*2 };
+        let ivb = if button.is_high() {
+            interval
+        } else {
+            interval * 3
+        };
         led.set_high();
         Timer::after(ivb).await;
         led.set_low();
@@ -21,7 +26,8 @@ async fn blinker(mut led: Output<'static>, button: Input<'static>, interval: Dur
 async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
+    info!("Starte");
     let led = Output::new(p.PB3, Level::Low, Speed::Low);
-    let button = Input::new(p.PB4,Pull::Down);
+    let button = Input::new(p.PB4, Pull::Down);
     let _ = spawner.spawn(blinker(led, button, Duration::from_millis(1_000)));
 }
